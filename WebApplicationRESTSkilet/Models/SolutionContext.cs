9e0e8405bb4cc81104b1c5e8ee247087d225fccd
Deverex <deverex@yandex.ru>
2019-08-05 00:50:12 +0300

    1

---
+++ WebApplicationRESTSkilet/Models/SolutionContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplicationRESTSkilet.Models
+{
+    public class SolutionContext : DbContext
+    {
+        public SolutionContext(DbContextOptions<SolutionContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<SolutionItem> SolutionItems { get; set; }
+    }
+
+}
---
+++ WebApplicationRESTSkilet/Models/SolutionItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplicationRESTSkilet.Models
+{
+    public class SolutionItem
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public int State { get; set; }
+    }
+}
